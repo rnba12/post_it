83444915cead445c7823101bc4a63991693d225b
rnba12 <rnba12@yahoo.co.uk>
2022-12-07 14:59:33 +0000

    Added model functions

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,7 +1,7 @@
-const init = require('../dbConfig');
-const ObjectId = require('mongodb')
+const { init } = require('../dbConfig');
+const {ObjectId} = require('mongodb')
 
-module.exports = class Post {
+class Post {
     constructor(data) {
         this.id = data.id
         this.title = data.title
@@ -11,7 +11,7 @@ module.exports = class Post {
     static get all() {
         return new Promise (async (resolve, reject) => {
             try {
-                const db = await init();
+                const db = await init()
                 const postsData = await db.collection('posts').find().toArray()
                 const posts = postsData.map(p => new Post({ ...p, id: p._id }))
                 resolve(posts)
@@ -27,9 +27,11 @@ module.exports = class Post {
             try {
                 const db = await init();
                 let postsData = await db.collection('posts').find({ _id: ObjectId(id) }).toArray()
+                console.log(postsData)
                 let post = new Post({...postsData[0], id: postsData[0]._id});
-                resolve (dog);
+                resolve (post);
             } catch (err) {
+                console.log(err)
                 reject('Post not found');
             }
         });
@@ -48,3 +50,5 @@ module.exports = class Post {
         });
     }
 }
+
+module.exports = Post;
