6fb4bc9ce600de3a4f567a552c085dd0a74a1d3c
adamminchella <adamminchella@msn.com>
2022-12-07 15:56:30 +0000

    Fetch and display data on result page

--- client/static/js/post.js
+++ client/static/js/post.js
@@ -4,10 +4,13 @@ const story = document.querySelector("#story");
 
 window.addEventListener("load", () => {
   const postId = window.location.href.split("=")[1];
-  const url = `http://localhost:3000/post/${postId}`;
+  const url = `http://localhost:3000/posts/${postId}`;
   fetch(url).then((res) =>
     res.json().then((data) => {
       console.log(data);
+      title.textContent = data.title;
+      author.textContent = data.author;
+      story.textContent = data.story;
     })
   );
 });
