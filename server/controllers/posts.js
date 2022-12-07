3dda44a1004c83e657885f18a9e97d3972b29c8e
adamminchella <adamminchella@msn.com>
2022-12-07 14:17:52 +0000

    Complete post controllers

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,31 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const Post = require("../models/Post");
 
 //index
-router.get('/', async (req, res) => {
-    try {
-        res.status(200).json({Message: 'All Posts'})
-    } catch (err) {
-        res.status(500).json({err});
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const allPosts = await Post.all;
+    res.status(200).json(allPosts);
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
 //show
-router.get('/:id', async (req, res) => {
-    try {
-        res.status(200).json({Message: 'Post by id'})
-    } catch (err) {
-        res.status(404).json({err});
-    }
-})
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
 
 //create
-router.post('/', async (req, res) => {
-    try {
-        
-    } catch (err) {
-        res.status(422).json({err})
-    }
-})
+router.post("/", async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body);
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(422).json({ err });
+  }
+});
 
 module.exports = router;
