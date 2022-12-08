c235b89c094de0843747c8d2c1b6d508b226311a
adamminchella <adamminchella@msn.com>
2022-12-08 17:34:02 +0000

    Add functionality to allow editing a post

--- client/static/js/post.js
+++ client/static/js/post.js
@@ -2,10 +2,12 @@ const title = document.querySelector("#title");
 const author = document.querySelector("#author");
 const story = document.querySelector("#story");
 const id = document.querySelector("#postId");
+const editBtn = document.querySelector("#submit");
+
+const postId = window.location.href.split("=")[1];
+const url = `http://localhost:3000/posts/${postId}`;
 
 window.addEventListener("load", () => {
-  const postId = window.location.href.split("=")[1];
-  const url = `http://localhost:3000/posts/${postId}`;
   fetch(url).then((res) =>
     res.json().then((data) => {
       console.log(data);
@@ -13,6 +15,15 @@ window.addEventListener("load", () => {
       author.textContent = `${data.author} - ${data.date}`;
       story.textContent = data.story;
       id.textContent = `ID - ${data.id}`;
+    })
+  );
+});
+
+editBtn.addEventListener("click", () => {
+  fetch(url).then((res) =>
+    res.json().then((data) => {
+      console.log(data);
+      window.location.href = `./edit.html?id=${data.id}`;
     })
   );
 });
